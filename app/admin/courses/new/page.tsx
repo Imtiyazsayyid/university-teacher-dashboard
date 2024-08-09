852eaa1b1095db7e12ea61949925793c57ccecde
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-08-09 14:15:05 +0530

    .

--- app/admin/courses/new/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CourseForm from "../Form";
-
-const NewCoursePage = () => {
-  return <CourseForm />;
-};
-
-export default NewCoursePage;
--- app/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HomePage = () => {
-  return <div className="h-full w-full flex justify-center items-center">Home Page</div>;
-};
-
-export default HomePage;

7a20916dda6c2e2597a522fbe9be16373fbd75bc
Fahad Khan <fahad.khan44604@gmail.com>
2025-04-21 20:09:41 +0530

    Fix a ui bug

--- app/teacher/chats/components/EmptyState.tsx
+++ app/teacher/chats/components/EmptyState.tsx
@@ -5,8 +5,8 @@ const EmptyState = () => {
     <div
       className="dark:bg-[#151515] px-4 py-10 sm:px-6 lg:px-10 h-full flex justify-center items-center bg-gray-100"
     >
-      <div className="flex flex-col text-center items-center">
-        <h3 className="mt-2 text-2xl font-semibold text-neutral-900">
+    <div className="flex flex-col text-center items-center">
+        <h3 className="mt-2 text-2xl font-semibold text-neutral-900 dark:text-white">
           Select a chat or start a new conversation
         </h3>
       </div>
